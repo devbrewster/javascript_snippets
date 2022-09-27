b186ba281234447da181591820e5e28084440c22
devbrewster <40252813+devbrewster@users.noreply.github.com>
2022-09-27 00:18:26 -0500

    added function section

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ if (Number(driverAge) < 18) {
 } else {
     alert("Powering On. Enjoy the ride!!!"); 
 }
+//-----------------------------------------------------------------------------------------------
+// function
+function checkDriverAge() {
+    var driverAge = prompt("Please enter your age to determine if you can drive");
+        if (Number(driverAge) < 18) {
+            alert("Sorry, you are too young to drive this car. Powering Off!!!");
+        } else if (Number(driverAge) === 18) {
+            alert("Congratulations on your first year of driving. Enjoy the ride!!!");
+        } else {
+          alert("Powering On. Enjoy the ride!!!");
+        }
+}
+
+//-----------------------------------------------------------------------------------------------
